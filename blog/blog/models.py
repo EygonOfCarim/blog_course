dd0cf112d4850cbbaf09ca466adbf0de13570dcd
eygonofcarim <pauloviniciustoledo@gmail.com>
2023-02-16 15:53:08 -0300

    Teste WebScraping

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-from datetime import datetime
 from django.utils import timezone
-# Create your models here.
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class Post(models.Model):
     autor = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
-    conteudo = models.TextField()
+    conteudo = RichTextUploadingField()
     data_publicacao = models.DateTimeField(default=timezone.now())
 
     def __str__(self):
