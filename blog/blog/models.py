9000cf2d2b1156601ea813a2dd2b91122ddc1daf
eygonofcarim <pauloviniciustoledo@gmail.com>
2023-02-23 16:57:23 -0300

    Refactoring {%%}

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,3 +1,4 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 from django.utils import timezone
 from ckeditor_uploader.fields import RichTextUploadingField
@@ -7,7 +8,9 @@ class Post(models.Model):
     autor = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
-    conteudo = RichTextUploadingField()
+    resumo = RichTextUploadingField(null=True, blank=True)
+    conteudo = RichTextField()
+    image_capa = models.ImageField(null=True, blank=True, upload_to='static/blog/')
     data_publicacao = models.DateTimeField(default=timezone.now())
 
     def __str__(self):
